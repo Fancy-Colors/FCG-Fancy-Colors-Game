2bfc345490cbfb8ebbf78fe7b8f3ac7af51f58f1
alex-kurkov <alexkourkov@yandex.ru>
2023-02-24 17:46:49 +0500

    Создал компонент Avatar

---
+++ packages/client/src/components/avatar/avatar.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import styles from './avatar.module.pcss';
+import cn from 'classnames';
+
+type Props = {
+  size: 'l' | 'm' | 's' | 'xs';
+  avatar?: string;
+  first_name: string;
+  label?: string;
+};
+
+export const Avatar: FC<Props> = ({ avatar, first_name, label, size }) => {
+  return (
+    <div
+      className={cn(styles.avatar, styles[size])}
+      style={{ backgroundImage: `url(${avatar})` || 'none' }}
+    >
+      {!avatar && (
+        <p>{first_name.length ? first_name[0].toUpperCase() : 'U'}</p>
+      )}
+      {label && <span className={cn(styles.label, 'text-menu')}>{label}</span>}
+    </div>
+  );
+};
