f0193cf593601e01ac1f6bbe75f5bf7560f9ee6f
alex-kurkov <alexkourkov@yandex.ru>
2023-02-25 11:18:52 +0500

    изменил название подчеркнутого поля first_name с API на консистентное name

--- packages/client/src/components/avatar/avatar.tsx
+++ packages/client/src/components/avatar/avatar.tsx
@@ -5,19 +5,17 @@ import cn from 'classnames';
 type Props = {
   size: 'l' | 'm' | 's' | 'xs';
   avatar?: string;
-  first_name: string;
+  name: string;
   label?: string;
 };
 
-export const Avatar: FC<Props> = ({ avatar, first_name, label, size }) => {
+export const Avatar: FC<Props> = ({ avatar, name, label, size }) => {
   return (
     <div
       className={cn(styles.avatar, styles[size])}
       style={{ backgroundImage: `url(${avatar})` }}
     >
-      {!avatar && (
-        <p>{first_name.length ? first_name[0].toUpperCase() : 'U'}</p>
-      )}
+      {!avatar && <p>{name.length ? name[0].toUpperCase() : 'U'}</p>}
       {label && <span className={cn(styles.label, 'text-menu')}>{label}</span>}
     </div>
   );
