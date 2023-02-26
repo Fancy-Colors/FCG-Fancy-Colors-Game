4ac2dccc3c9f26e075e759f11c25cfbd58bf5971
alex-kurkov <alexkourkov@yandex.ru>
2023-02-26 11:39:58 +0500

    Дeкларирует IconType глобально

---
+++ packages/client/typings/app.d.ts
@@ -0,0 +1,20 @@
+declare global {
+  export type IconType =
+    | 'main'
+    | 'leaderboard'
+    | 'forum'
+    | 'settings'
+    | 'close'
+    | 'vk'
+    | 'search'
+    | 'arrow'
+    | 'circle'
+    | 'enter'
+    | 'exit'
+    | 'github'
+    | 'message'
+    | 'smile'
+    | 'telegram';
+}
+
+export {};
