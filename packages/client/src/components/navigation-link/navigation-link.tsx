77da3d621f612f15d10c04df531dd75c33be1c4f
alex-kurkov <alexkourkov@yandex.ru>
2023-02-26 16:06:49 +0500

    Добавляет компонент NavigationLink

--- packages/client/src/components/navigation-link/navigation-link.tsx
+++ packages/client/src/components/navigation-link/navigation-link.tsx
@@ -3,19 +3,36 @@ import styles from './navigation-link.module.pcss';
 import cn from 'classnames';
 import { Icon } from 'components/icon';
 
+const ICON_COLOR = '#0f101b';
+
 type Props = {
-  expanded: boolean;
-  type: 'main' | 'leaderboard' | 'forum';
+  size: 's' | 'l';
+  iconType: IconType;
   active: boolean;
   text: string;
   informer?: string;
-};
+} & JSX.IntrinsicElements['button'];
 
-export const NavigationLink: FC<Props> = ({ type, active, text }) => {
+export const NavigationLink: FC<Props> = ({
+  iconType,
+  active,
+  text,
+  informer,
+  size = 'l',
+}) => {
   return (
-    <div className={cn(styles.navigation, { [styles.active]: active })}>
-      <Icon type={type} size="small" />
-      {text}
+    <div
+      className={cn(styles.navigation, styles[size], {
+        [styles.active]: active,
+      })}
+    >
+      <Icon type={iconType} size="small" color={ICON_COLOR} />
+      {size === 'l' && (
+        <div className={styles.textAndInfoWrap}>
+          <p className={cn(styles.text, 'text-main')}>{text}</p>
+          {informer && <div className={styles.informer}>{informer}</div>}
+        </div>
+      )}
     </div>
   );
 };
