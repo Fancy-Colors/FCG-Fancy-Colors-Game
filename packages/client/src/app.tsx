d38835f66adfc2d80549bcff670d764c2c0607c6
Aleksa-esme <esme1990@mail.ru>
2023-02-28 15:36:48 +0300

    Отдельно вынесены названия путей

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,6 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { TestPage } from './pages/test';
 
+export enum Paths {
+  main = '/',
+  howto = 'how-to',
+  register = 'sign-up',
+  login = 'login',
+  profile = 'profile',
+  game = 'game/:id',
+  leaderboard = 'leaderboard',
+  forum = 'forum',
+  new_thread = 'forum/new',
+  thread = 'forum/:id',
+  error500 = '500',
+  error404 = '404',
+}
+
 // если у страницы есть дочерний роут - не забудьте при верстке указать компонент <Outlet />
 
 function App() {
@@ -8,25 +23,44 @@ function App() {
     <BrowserRouter>
       <div>
         <Routes>
-          <Route path="/" element={<TestPage text="Главная" />}>
+          <Route path={Paths.main} element={<TestPage text="Главная" />}>
             <Route
-              path="howto"
+              path={Paths.howto}
               element={<TestPage text='Модалка "Как играть"' />}
             />
           </Route>
-          <Route path="register" element={<TestPage text="Регистрация" />} />
-          <Route path="login" element={<TestPage text="Логин" />} />
-          <Route path="profile" element={<TestPage text="Профиль" />} />
-          <Route path=":id" element={<TestPage text="Страница игры" />} />
-          <Route path="leaders" element={<TestPage text="Лидерборд" />} />
-          <Route path="forum" element={<TestPage text="Форум" />}>
+          <Route
+            path={Paths.register}
+            element={<TestPage text="Регистрация" />}
+          />
+          <Route path={Paths.login} element={<TestPage text="Логин" />} />
+          <Route path={Paths.profile} element={<TestPage text="Профиль" />} />
+          <Route
+            path={Paths.game}
+            element={<TestPage text="Страница игры" />}
+          />
+          <Route
+            path={Paths.leaderboard}
+            element={<TestPage text="Лидерборд" />}
+          />
+          <Route path={Paths.forum} element={<TestPage text="Форум" />}>
             <Route
-              path="forum/create"
+              path={Paths.new_thread}
               element={<TestPage text='Модалка "Создать тему"' />}
             />
           </Route>
-          <Route path="forum/:id" element={<TestPage text="Тема форума" />} />
-          <Route path="error" element={<TestPage text="Ошибка 500" />} />
+          <Route
+            path={Paths.thread}
+            element={<TestPage text="Тема форума" />}
+          />
+          <Route
+            path={Paths.error500}
+            element={<TestPage text="Ошибка 500" />}
+          />
+          <Route
+            path={Paths.error404}
+            element={<TestPage text="Нет такой страницы" />}
+          />
           <Route path="*" element={<TestPage text="Нет такой страницы" />} />
         </Routes>
       </div>
