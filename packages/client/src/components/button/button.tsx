aef2d22367c28ebc12a81975d2866984f276d6b9
Aleksa-esme <esme1990@mail.ru>
2023-02-28 15:15:08 +0300

    компоненты разделены на Button и ButtonLink

--- packages/client/src/components/button/button.tsx
+++ packages/client/src/components/button/button.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, LinkProps } from 'react-router-dom';
 import cn from 'classnames';
 import styles from './button.module.pcss';
 
@@ -17,16 +17,22 @@ const ButtonColorChoice = {
   [ButtonColor.icon]: styles.icon,
 };
 
-type ButtonProps = {
-  link?: string;
+type Props = {
   color?: keyof typeof ButtonColor;
   size?: 'small' | 'medium' | 'large';
   className?: string;
   children: JSX.Element | JSX.Element[] | string;
-} & JSX.IntrinsicElements['button'];
+};
 
-export const Button: FC<ButtonProps> = ({
-  link,
+type ButtonAsLink = {
+  to: string;
+} & Props &
+  LinkProps;
+
+type ButtonAsButton = Props & JSX.IntrinsicElements['button'];
+
+export const Button: FC<ButtonAsButton> = ({
+  type = 'button',
   color = ButtonColor.colored,
   size = 'medium',
   className,
@@ -41,16 +47,30 @@ export const Button: FC<ButtonProps> = ({
   );
 
   return (
-    <>
-      {link ? (
-        <Link to={link} className={cnButton}>
-          <span className={styles.btn}>{children}</span>
-        </Link>
-      ) : (
-        <button className={cnButton} {...props}>
-          <span className={styles.btn}>{children}</span>
-        </button>
-      )}
-    </>
+    <button type={type} className={cnButton} {...props}>
+      <span className={styles.btn}>{children}</span>
+    </button>
+  );
+};
+
+export const ButtonLink: FC<ButtonAsLink> = ({
+  to,
+  color = ButtonColor.colored,
+  size = 'medium',
+  className,
+  children,
+  ...props
+}) => {
+  const cnButton = cn(
+    className,
+    styles.button,
+    styles[size],
+    ButtonColorChoice[color] ?? styles.colored
+  );
+
+  return (
+    <Link to={to} className={cnButton} {...props}>
+      <span className={styles.btn}>{children}</span>
+    </Link>
   );
 };
--- packages/client/src/components/button/index.ts
+++ packages/client/src/components/button/index.ts
@@ -1,1 +1,1 @@
-export { Button } from './button';
+export { Button, ButtonLink } from './button';
