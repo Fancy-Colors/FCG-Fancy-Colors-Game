702211249b1b63ed956a197f123170e456820d16
Alexey Kurkov <65237410+alex-kurkov@users.noreply.github.com>
2023-03-01 17:10:22 +0500

    Добавляет `SideMenu` (#15)
    * initial setup
    
    * update & lock dependencies
    
    * add fonts
    
    * Добавил описание работы с иконками
    
    * Добавил в игнор css в папке dist
    
    * add .yarn to gitignore
    
    * add DateFormatted Component and normalizer utility
    
    * kebabized util & remove magic nums
    
    * remove unnecesary type from props
    
    * add global styles and vars
    
    * реэкспорт компонента через index.ts
    
    * Создал компонент `tabs`
    
    * сузил игнорирование правил stylelint в глоб css-файлах
    
    * добавил компонент Icon для удобства работы с иконками и их цветом/размером по всему приложению
    
    * Добавил компонент `TextField` (#5)
    
    Мерж компонента TextField
    
    * Создал компонент Avatar (#6)
    
    * Создал компонент Avatar
    
    * убрал лишнее альтернативное присваивания в инлайновом стиле
    
    * изменил название подчеркнутого поля first_name с API на консистентное name
    
    * Lib/error stub/90 (#7)
    
    * lib/error-stub/90. Добавляет компонент заглушку при ошибке.
    
    * lib/error-stub/90. Добавляет компонент заглушку при ошибке.
    
    * refactoring. Согласно КР из https://github.com/Fancy-Colors/FCG-Fancy-Colors-Game/pull/7
    
    ---------
    
    Co-authored-by: mozg3000 <lykosh@positron-it,ru>
    
    * Начальная настройка компонента NavigationLink
    
    * перерисовал иконки и сократил код + 3 новых
    
    * добавил все иконки и новые размеры
    
    * Добавил компонент ProfileLink
    
    * убрал инлайновый fill
    
    * правки и улучшения
    
    * Дeкларирует IconType глобально
    
    * Исправляет tsconfig для исключения потери типов установленных пакетов
    
    * Добавляет компонент NavigationLink
    
    * убрал ненужные пропсы кнопки
    
    * Добавляет Footer
    
    * изменил формат передачи size
    
    * Заменяет пропс size на более подходящий expanded
    
    * Заменяет пропс size на более подходящий expanded
    
    * Заменяет пропс size на более подходящий expanded
    
    * Удаляет ненужное выключение правила stylelint
    
    * Заменяет возможные значения пропса size на полные слова
    
    * корректировка названия пропсов и правка ссылки
    
    * изменил size на expanded
    
    * Добавляет SideMenu
    
    * выравнивает контент футера
    
    * убрал ненужный импорт
    
    * поправил стили
    
    * поправил стили 2
    
    ---------
    
    Co-authored-by: DNLHC <gnz.danila@gmail.com>
    Co-authored-by: Danila <MachineIsComing@gmail.com>
    Co-authored-by: mozg3000 <45194783+mozg3000@users.noreply.github.com>
    Co-authored-by: mozg3000 <lykosh@positron-it,ru>
    Co-authored-by: Андрей Студенов <marmeloy@MBP-Andrej.Dlink>
    Co-authored-by: Andrew Studenov <adnrew.studenov@ya.ru>

---
+++ packages/client/src/components/side-menu/side-menu.tsx
@@ -0,0 +1,150 @@
+import { FC, useState } from 'react';
+import styles from './side-menu.module.pcss';
+import cn from 'classnames';
+import { NavigationLink } from 'components/navigation-link';
+import { Link } from 'react-router-dom';
+import { ProfileLink } from 'components/profile-link';
+import { Footer } from 'components/footer';
+import { ReactComponent as Logo } from 'assets/logo.svg';
+import { ReactComponent as LogoNarrow } from 'assets/logo-narrow.svg';
+import { Icon } from 'components/icon';
+
+type LinkType = 'forum' | 'main' | 'profile' | 'forum' | 'leaderboard' | '';
+type Link = {
+  iconType: IconType;
+  link: LinkType;
+  text: string;
+  informer?: string;
+};
+
+// пока что хардкод...
+const LOGO_COLOR = '#6644ec';
+const LINKS: Link[] = [
+  {
+    iconType: 'main',
+    link: 'main',
+    text: 'Главная',
+  },
+  {
+    iconType: 'leaderboard',
+    link: 'leaderboard',
+    text: 'Лидерборд',
+  },
+  {
+    iconType: 'forum',
+    link: 'forum',
+    text: 'Форум',
+    informer: '23',
+  },
+];
+const EXIT: Link = {
+  iconType: 'exit',
+  text: 'Выйти',
+  link: '',
+};
+const PROFILE = {
+  label: 'Профиль',
+  link: 'profile' as LinkType,
+  email: 'johnson@ya.ru',
+  name: 'Иван Джонсон',
+  avatar:
+    'https://avatars.mds.yandex.net/i?id=2a00000179f132be486a8a744b50b9b49ec1-5025855-images-thumbs&n=13&exp=1',
+};
+const SOCIAL_LINKS = [
+  {
+    icon: 'github' as IconType,
+    link: 'https://github.com/Fancy-Colors',
+  },
+  {
+    icon: 'vk' as IconType,
+    link: '#',
+  },
+  {
+    icon: 'telegram' as IconType,
+    link: 'https://t.me',
+  },
+];
+
+export const SideMenu: FC = () => {
+  const [activeLink, setActiveLink] = useState(LINKS[0].link);
+  const [expanded, setExpanded] = useState(true);
+
+  return (
+    <section
+      className={cn(styles.sideMenu, {
+        ['w-4']: expanded,
+        ['w-1']: !expanded,
+      })}
+    >
+      <nav
+        className={cn(styles.nav, {
+          ['w-3']: expanded,
+          ['w-1']: !expanded,
+        })}
+      >
+        <Link to="main" onClick={() => setActiveLink('main')}>
+          <div className={styles.logo}>
+            {expanded ? (
+              <Logo width="100%" height="100%" fill={LOGO_COLOR} />
+            ) : (
+              <LogoNarrow width="100%" height="100%" fill={LOGO_COLOR} />
+            )}
+          </div>
+        </Link>
+
+        <div className={styles.mainLinksWrap}>
+          {expanded && <p className={cn(styles.textMenu, 'text-menu')}>Меню</p>}
+          {LINKS.map(({ iconType, text, link, informer }) => (
+            <Link key={iconType} to={link} onClick={() => setActiveLink(link)}>
+              <NavigationLink
+                text={text}
+                iconType={iconType}
+                active={link === activeLink}
+                expanded={expanded}
+                informer={informer}
+              />
+            </Link>
+          ))}
+        </div>
+
+        {expanded && <div className={styles.delimiter} />}
+
+        <Link to="profile" onClick={() => setActiveLink('profile')}>
+          <ProfileLink {...PROFILE} expanded={expanded} />
+        </Link>
+
+        {expanded && (
+          <>
+            <div className={styles.delimiter} />
+            <Link to="main/howto" className="text-main">
+              Как играть в Fancy Colors?
+            </Link>
+          </>
+        )}
+        <button
+          type="button"
+          className={cn(styles.menuToggler, { [styles.expanded]: expanded })}
+          onClick={() => setExpanded(!expanded)}
+        >
+          <Icon type="arrow" size="medium" color="#0f101b" />
+        </button>
+      </nav>
+      <div>
+        <button
+          className={cn(styles.exit, {
+            ['w-3']: expanded,
+            ['w-1']: !expanded,
+          })}
+        >
+          <NavigationLink
+            text={EXIT.text}
+            iconType={EXIT.iconType}
+            active={EXIT.link === activeLink}
+            expanded={expanded}
+          />
+        </button>
+        <Footer expanded={expanded} links={SOCIAL_LINKS} />
+      </div>
+    </section>
+  );
+};
