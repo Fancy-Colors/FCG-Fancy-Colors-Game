a56efe6d0490ef8d8ee6c499f6f67b8b1832df1d
Aleksa-esme <esme1990@mail.ru>
2023-03-14 09:19:34 +0300

    Добавлен service worker

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -25,6 +25,18 @@ export default defineConfig({
     // eslint-disable-next-line @typescript-eslint/naming-convention
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
+  build: {
+    emptyOutDir: true,
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[ext]/[name][extname]',
+        chunkFileNames: 'assets/[chunks]/[name].[hash].js',
+        entryFileNames: 'assets/js/main.js',
+      },
+    },
+  },
   plugins: [
     react(),
     svgr({
