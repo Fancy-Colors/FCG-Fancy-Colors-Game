fde6418c4a3ab365779f7cd474262860ee7f942d
Aleksandra <58600329+Aleksa-esme@users.noreply.github.com>
2023-03-27 13:20:50 +0300

    адаптация лидерборда и сайдменю под мобильные устройства (#53)

--- packages/client/src/components/hooks/index.ts
+++ packages/client/src/components/hooks/index.ts
@@ -3,3 +3,4 @@ export { useAppDispatch } from './use-app-dispatch';
 export { useAppSelector } from './use-app-selector';
 export { useAuth } from './use-auth';
 export { useTheme } from './use-theme';
+export { useWindowSize } from './use-window-size';
--- packages/client/src/pages/leaderboard/leaderboard.tsx
+++ packages/client/src/pages/leaderboard/leaderboard.tsx
@@ -1,5 +1,6 @@
 import styles from './leaderboard.module.pcss';
 import { Leader } from 'components/leader';
+import { ErrorBoundary } from 'utils/error-boundary';
 import { TLeader, LEADERS, FILTERED_LEADERS } from 'src/mock/leaders';
 import cn from 'classnames';
 
@@ -7,36 +8,44 @@ const CURRENT_USER_LOGIN = 'user_login';
 
 export const Leaderboard = () => {
   return (
-    <div className={cn(styles.leaderboard, 'u-page', 'u-fancy-scrollbar')}>
-      <div className={styles.topLeaders}>
-        {LEADERS.length > 1 && <Leader {...LEADERS[1]} size="small" />}
-        {LEADERS.length > 0 && <Leader {...LEADERS[0]} size="medium" />}
-        {LEADERS.length > 2 && <Leader {...LEADERS[2]} size="small" />}
+    <section className={cn(styles.container, 'u-page')}>
+      <div className={cn(styles.leaderboard, 'u-fancy-scrollbar')}>
+        <div className={cn(styles.content)}>
+          <div className={styles.topLeaders}>
+            <ErrorBoundary>
+              {LEADERS.length > 1 && <Leader {...LEADERS[1]} size="small" />}
+              {LEADERS.length > 0 && <Leader {...LEADERS[0]} size="medium" />}
+              {LEADERS.length > 2 && <Leader {...LEADERS[2]} size="small" />}
+            </ErrorBoundary>
+          </div>
+          <div className={styles.leadersList}>
+            {LEADERS.slice(3).map((leader: TLeader, key: number) => {
+              return (
+                <ErrorBoundary key={key}>
+                  <Leader
+                    {...leader}
+                    size="row"
+                    active={leader.login === CURRENT_USER_LOGIN}
+                  />
+                </ErrorBoundary>
+              );
+            })}
+          </div>
+          <div className={styles.leadersList}>
+            {FILTERED_LEADERS.map((leader: TLeader, key: number) => {
+              return (
+                <ErrorBoundary key={key}>
+                  <Leader
+                    {...leader}
+                    size="row"
+                    active={leader.login === CURRENT_USER_LOGIN}
+                  />
+                </ErrorBoundary>
+              );
+            })}
+          </div>
+        </div>
       </div>
-      <div className={styles.leadersList}>
-        {LEADERS.slice(3).map((leader: TLeader, key: number) => {
-          return (
-            <Leader
-              {...leader}
-              size="row"
-              key={key}
-              active={leader.login === CURRENT_USER_LOGIN}
-            />
-          );
-        })}
-      </div>
-      <div className={styles.leadersList}>
-        {FILTERED_LEADERS.map((leader: TLeader, key: number) => {
-          return (
-            <Leader
-              {...leader}
-              size="row"
-              key={key}
-              active={leader.login === CURRENT_USER_LOGIN}
-            />
-          );
-        })}
-      </div>
-    </div>
+    </section>
   );
 };
