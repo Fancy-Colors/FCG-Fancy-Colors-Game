8bc4d1227f37444f147ebb1db587dc60cab6dcac
alex-kurkov <alexkourkov@yandex.ru>
2023-03-28 20:16:42 +0500

    добавил переход на модалку с правилами через search params

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -5,11 +5,11 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
+  LoaderFunction,
 } from 'react-router-dom';
 import { ProtectedRoutes } from 'utils/protected-routes';
 import { Profile } from 'pages/profile';
 import { MainPage } from 'pages/main';
-import { HowToModal } from 'components/how-to-modal';
 import { GamePage } from 'pages/game';
 import { RouterPaths } from './app.types';
 import { LoginPage, RegisterPage } from 'pages/auth';
@@ -20,36 +20,39 @@ import { ForumThread } from 'pages/forum-thread';
 import { NewThreadModal } from 'components/modal-new-thread';
 import { Forum } from 'pages/forum';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route
-      element={<AuthLayout />}
-      loader={() => defer({ userPromise: getCurrentUser() })}
-    >
-      <Route path={RouterPaths.REGISTER} element={<RegisterPage />} />
-      <Route path={RouterPaths.LOGIN} element={<LoginPage />} />
-      <Route element={<MainLayout />}>
-        <Route path={RouterPaths.MAIN} element={<MainPage />}>
-          <Route path={RouterPaths.HOW_TO} element={<HowToModal />} />
-        </Route>
-        <Route path={RouterPaths.LEADERBOARD} element={<Leaderboard />} />
-        <Route path={RouterPaths.FORUM} element={<Forum />}>
-          <Route path={RouterPaths.NEW_THREAD} element={<NewThreadModal />} />
-        </Route>
-        <Route path={`${RouterPaths.FORUM}/:id`} element={<ForumThread />} />
-        <Route element={<ProtectedRoutes />}>
-          <Route path={RouterPaths.PROFILE} element={<Profile />} />
-          <Route path={`${RouterPaths.GAME}/:id`} element={<GamePage />} />
-        </Route>
-      </Route>
-      <Route path={RouterPaths.ERROR_500} element={<Error500 />} />
-      <Route path={RouterPaths.ERROR_404} element={<Error404 />} />
-      <Route path="*" element={<Error404 />} />
-    </Route>
-  )
-);
-
 function App() {
+  const modalLoader: LoaderFunction = ({ request }) => {
+    const url = new URL(request.url);
+    return url.searchParams.get('modal');
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route
+        element={<AuthLayout />}
+        loader={() => defer({ userPromise: getCurrentUser() })}
+      >
+        <Route path={RouterPaths.REGISTER} element={<RegisterPage />} />
+        <Route path={RouterPaths.LOGIN} element={<LoginPage />} />
+        <Route element={<MainLayout />} loader={modalLoader}>
+          <Route path={RouterPaths.MAIN} element={<MainPage />} />
+          <Route path={RouterPaths.LEADERBOARD} element={<Leaderboard />} />
+          <Route path={RouterPaths.FORUM} element={<Forum />}>
+            <Route path={RouterPaths.NEW_THREAD} element={<NewThreadModal />} />
+          </Route>
+          <Route path={`${RouterPaths.FORUM}/:id`} element={<ForumThread />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path={RouterPaths.PROFILE} element={<Profile />} />
+            <Route path={`${RouterPaths.GAME}/:id`} element={<GamePage />} />
+          </Route>
+        </Route>
+        <Route path={RouterPaths.ERROR_500} element={<Error500 />} />
+        <Route path={RouterPaths.ERROR_404} element={<Error404 />} />
+        <Route path="*" element={<Error404 />} />
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />;
 }
 
