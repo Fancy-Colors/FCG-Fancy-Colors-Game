579a36aea85425705ecd1afdddca07376af816d5
alex-kurkov <alexkourkov@yandex.ru>
2023-03-28 20:22:23 +0500

    вернул App в первозданное состояние

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -5,7 +5,6 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
-  LoaderFunction,
 } from 'react-router-dom';
 import { ProtectedRoutes } from 'utils/protected-routes';
 import { Profile } from 'pages/profile';
@@ -20,39 +19,40 @@ import { ForumThread } from 'pages/forum-thread';
 import { NewThreadModal } from 'components/modal-new-thread';
 import { Forum } from 'pages/forum';
 
-function App() {
-  const modalLoader: LoaderFunction = ({ request }) => {
-    const url = new URL(request.url);
-    return url.searchParams.get('modal');
-  };
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      element={<AuthLayout />}
+      loader={() => defer({ userPromise: getCurrentUser() })}
+    >
+      <Route path={RouterPaths.REGISTER} element={<RegisterPage />} />
+      <Route path={RouterPaths.LOGIN} element={<LoginPage />} />
       <Route
-        element={<AuthLayout />}
-        loader={() => defer({ userPromise: getCurrentUser() })}
+        element={<MainLayout />}
+        loader={({ request }) => {
+          const url = new URL(request.url);
+          return url.searchParams.get('modal');
+        }}
       >
-        <Route path={RouterPaths.REGISTER} element={<RegisterPage />} />
-        <Route path={RouterPaths.LOGIN} element={<LoginPage />} />
-        <Route element={<MainLayout />} loader={modalLoader}>
-          <Route path={RouterPaths.MAIN} element={<MainPage />} />
-          <Route path={RouterPaths.LEADERBOARD} element={<Leaderboard />} />
-          <Route path={RouterPaths.FORUM} element={<Forum />}>
-            <Route path={RouterPaths.NEW_THREAD} element={<NewThreadModal />} />
-          </Route>
-          <Route path={`${RouterPaths.FORUM}/:id`} element={<ForumThread />} />
-          <Route element={<ProtectedRoutes />}>
-            <Route path={RouterPaths.PROFILE} element={<Profile />} />
-            <Route path={`${RouterPaths.GAME}/:id`} element={<GamePage />} />
-          </Route>
+        <Route path={RouterPaths.MAIN} element={<MainPage />} />
+        <Route path={RouterPaths.LEADERBOARD} element={<Leaderboard />} />
+        <Route path={RouterPaths.FORUM} element={<Forum />}>
+          <Route path={RouterPaths.NEW_THREAD} element={<NewThreadModal />} />
         </Route>
-        <Route path={RouterPaths.ERROR_500} element={<Error500 />} />
-        <Route path={RouterPaths.ERROR_404} element={<Error404 />} />
-        <Route path="*" element={<Error404 />} />
+        <Route path={`${RouterPaths.FORUM}/:id`} element={<ForumThread />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path={RouterPaths.PROFILE} element={<Profile />} />
+          <Route path={`${RouterPaths.GAME}/:id`} element={<GamePage />} />
+        </Route>
       </Route>
-    )
-  );
+      <Route path={RouterPaths.ERROR_500} element={<Error500 />} />
+      <Route path={RouterPaths.ERROR_404} element={<Error404 />} />
+      <Route path="*" element={<Error404 />} />
+    </Route>
+  )
+);
 
+function App() {
   return <RouterProvider router={router} />;
 }
 
