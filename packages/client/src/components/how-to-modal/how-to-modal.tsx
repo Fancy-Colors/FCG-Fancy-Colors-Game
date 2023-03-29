805c054777dfbae0fd2ab2f4228a7633eb97e4e8
alex-kurkov <alexkourkov@yandex.ru>
2023-03-29 08:47:46 +0500

    исправлена навигация в случае попадания на роут с модалкой по прямой ссылке

--- packages/client/src/components/how-to-modal/how-to-modal.tsx
+++ packages/client/src/components/how-to-modal/how-to-modal.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Modal } from 'components/modal';
 import { howToTextData } from 'src/mock/how-to-data';
 import cn from 'classnames';
@@ -7,9 +7,10 @@ import styles from './how-to-modal.module.pcss';
 
 export const HowToModal: FC = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   return (
-    <Modal onClose={() => navigate(-1)}>
+    <Modal onClose={() => navigate(pathname)}>
       <div className={cn(styles.content, 'u-fancy-scrollbar', 'w-6')}>
         {howToTextData.map(({ title, text }, idx) => {
           return (
