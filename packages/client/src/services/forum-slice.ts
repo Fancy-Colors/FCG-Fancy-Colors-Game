275c253a0e1a0a6723564216c62abd20db46d630
Marmeloy <andrew.studenov@ya.ru>
2023-05-16 22:14:48 +0300

    Реализация получения и создания сообщений треда (#72)
    * Реализация получения и создания сообщений треда
    
    * исправил параметры BaseApi
    
    * исправил параметры BaseApi в messages api
    
    ---------
    
    Co-authored-by: Andrew Studenov <adnrew.studenov@ya.ru>
    Co-authored-by: DNLHC <gnz.danila@gmail.com>

--- packages/client/src/services/forum-slice.ts
+++ packages/client/src/services/forum-slice.ts
@@ -4,10 +4,12 @@ export type Forum = ForumItem[];
 
 export type ForumMessage = {
   id: number;
-  avatar?: string;
-  name: string;
-  date: string;
   text: string;
+  createdAt: string;
+  updatedAt: string;
+  threadId: string;
+  repliedTo: string;
+  createdBy: number;
 };
 
 export type ForumItem = {
@@ -22,7 +24,9 @@ export type ForumItem = {
 export type Thread = {
   id: number;
   title: string;
-  messages: ForumMessage[];
+  createdBy: number;
+  createdAt: string;
+  updatedAt: string;
 };
 
 export type ForumState = {
@@ -31,6 +35,9 @@ export type ForumState = {
   currentForumPage: number;
   currentThreadPage: number;
   thread: Thread;
+  messages: ForumMessage[];
+  messagesCount: number;
+  messagesPagesCount: number;
 };
 
 const initialState: ForumState = {
@@ -39,8 +46,13 @@ const initialState: ForumState = {
   thread: {
     id: -1,
     title: '',
-    messages: [],
+    createdBy: -1,
+    createdAt: '',
+    updatedAt: '',
   },
+  messages: [],
+  messagesCount: 0,
+  messagesPagesCount: 0,
   currentForumPage: 1,
   currentThreadPage: 1,
 };
@@ -68,6 +80,32 @@ export const forumSlice = createSlice({
     ) => {
       state.thread = action.payload.data;
     },
+    setMessages: (
+      state,
+      action: PayloadAction<{
+        threadId: number;
+        page: number;
+        data: ForumMessage[];
+      }>
+    ) => {
+      state.messages = action.payload.data;
+    },
+    setMessagesCount: (
+      state,
+      action: PayloadAction<{
+        count: number;
+      }>
+    ) => {
+      state.messagesCount = action.payload.count;
+    },
+    setMessagesPagesCount: (
+      state,
+      action: PayloadAction<{
+        count: number;
+      }>
+    ) => {
+      state.messagesPagesCount = action.payload.count;
+    },
     setCurrentForumPage: (state, action: PayloadAction<{ page: number }>) => {
       state.currentForumPage = action.payload.page;
     },
@@ -80,6 +118,9 @@ export const forumSlice = createSlice({
 export const {
   setForum,
   setThread,
+  setMessages,
+  setMessagesCount,
+  setMessagesPagesCount,
   setCurrentForumPage,
   setCurrentThreadPage,
 } = forumSlice.actions;
