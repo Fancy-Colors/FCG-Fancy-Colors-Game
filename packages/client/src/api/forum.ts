f11fddc4c37866b2572e65456d7f0d9d080d07b9
DNLHC <gnz.danila@gmail.com>
2023-05-16 22:09:10 +0300

    исправил параметры BaseApi

--- packages/client/src/api/forum.ts
+++ packages/client/src/api/forum.ts
@@ -1,12 +1,9 @@
 import { threads } from 'src/mock/forum-threads';
 import { ForumItem as ForumItemProps, Thread } from 'src/services/forum-slice';
 import { BaseApi } from 'api/base';
+import { localHttpClient } from './api-clients';
 
 class ForumApi extends BaseApi {
-  constructor() {
-    super('/threads', import.meta.env.VITE_LOCAL_API_BASE_URL);
-  }
-
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   getForum = (pageNumber: string | number = 1): Promise<ForumItemProps[]> => {
     return new Promise<ForumItemProps[]>((resolve) => {
@@ -19,4 +16,4 @@ class ForumApi extends BaseApi {
   }
 }
 
-export const forumApi = new ForumApi();
+export const forumApi = new ForumApi(localHttpClient('/threads'));
