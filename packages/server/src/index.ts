9753d6b315fb896a030b183a555ff458a555267c
Danila <MachineIsComing@gmail.com>
2023-05-16 10:15:29 +0300

    CSP заголовки (#71)
    * добавил csp заголовки через helmet
    
    * csp для дев режима
    
    * рефакторинг
    
    * опечатка

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -8,6 +8,9 @@ import cookieParser from 'cookie-parser';
 import { dbConnect } from './db.js';
 import { router } from './router.js';
 import { createSSRController } from './controllers/ssr.controller.js';
+import helmet from 'helmet';
+import { cspNonce } from './middlewares/csp-nonce.js';
+import type { IncomingMessage, ServerResponse } from 'node:http';
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -23,6 +26,7 @@ async function bootstrap() {
 
   let vite: ViteDevServer | undefined;
 
+  app.disable('x-powered-by');
   app.use(cors());
 
   if (isDev) {
@@ -54,6 +58,42 @@ async function bootstrap() {
     app.use(express.static(staticPath, { index: false }));
   }
 
+  app.use(cspNonce);
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          scriptSrc: [
+            "'self'",
+            ...(!isDev
+              ? [
+                  (_: IncomingMessage, res: ServerResponse) =>
+                    `'nonce-${(res as express.Response).locals.cspNonce}'`,
+                  // service worker inline-script
+                  "'sha256-JKlrQLtbQcmSH0oVBT5qIkf0mOtxyMfcbvu+h4lHFeE='",
+                ]
+              : ["'unsafe-inline'"]), // Vite не поддерживает nonce
+          ],
+          imgSrc: [
+            "'self'",
+            'data:',
+            'avatars.mds.yandex.net',
+            'fancy-api.kurkov.online',
+          ],
+          objectSrc: "'none'",
+          connectSrc: [
+            "'self'",
+            'fancy-api.kurkov.online',
+            'ya-praktikum.tech',
+            // Vite dev server
+            ...(isDev ? ['ws:', 'http:'] : []),
+          ],
+        },
+      },
+      xDnsPrefetchControl: { allow: true },
+      xPoweredBy: false,
+    })
+  );
   app.use('*', cookieParser(), createSSRController(vite));
 
   app.listen(port, () => {
